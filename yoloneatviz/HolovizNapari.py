a96dd5c4aa38b79bf7fcd0eca3fd16561961cc94
kapoorlab <randomaccessiblekapoor@gmail.com>
2021-08-05 16:34:33 +0200

    csv triggers

--- yoloneatviz/HolovizNapari.py
+++ yoloneatviz/HolovizNapari.py
@@ -42,9 +42,12 @@ class NEATViz(object):
                self.fileextension = fileextension
                Path(self.savedir).mkdir(exist_ok=True)
                self.viewer = napari.Viewer()
-               
+               self.time = 0
                self.load_json()
                self.key_categories = self.load_json()
+               
+               
+               
                self.showNapari()
                
         def load_json(self):
@@ -78,151 +81,116 @@ class NEATViz(object):
                      imageidbox.addItem(str(Imageids[i]))
                      
                      
-                 figure = plt.figure(figsize=(4, 4))
-                 multiplot_widget = FigureCanvas(figure)
-                 ax = multiplot_widget.figure.subplots(1, 1)
+                 self.figure = plt.figure(figsize=(4, 4))
+                 self.multiplot_widget = FigureCanvas(self.figure)
+                 self.ax = self.multiplot_widget.figure.subplots(1, 1)
                  width = 400
                  dock_widget = self.viewer.window.add_dock_widget(
-                 multiplot_widget, name="EventStats", area='right')
-                 multiplot_widget.figure.tight_layout()
-                 self.viewer.window._qt_window.resizeDocks([dock_widget], [width], Qt.Horizontal)    
-                 eventidbox.currentIndexChanged.connect(lambda eventid = eventidbox : EventViewer(
-                         self.viewer,
-                         eventidbox.currentText(),
-                         self.key_categories,
+                 self.multiplot_widget, name="EventStats", area='right')
+                 self.multiplot_widget.figure.tight_layout()
+                 self.viewer.window._qt_window.resizeDocks([dock_widget], [width], Qt.Horizontal)   
+                 
+                 
+                 eventidbox.currentIndexChanged.connect(lambda eventid = eventidbox : self.csv_add(
+                         
+                         
                          os.path.basename(os.path.splitext(imageidbox.currentText())[0]),
-                         self.savedir,
-                         multiplot_widget,
-                         ax,
-                         figure
+                         eventidbox.currentText()
                     
                 )
             )    
                  
                  imageidbox.currentIndexChanged.connect(
-                 lambda trackid = imageidbox: EventViewer(
-                         self.viewer,
+                 lambda trackid = imageidbox: self.image_add(
+                         
                          imageidbox.currentText(),
-                         self.key_categories,
-                         os.path.basename(os.path.splitext(imageidbox.currentText())[0]),
-                         self.savedir,
-                         multiplot_widget,
-                         ax,
-                         figure
+                         
+                         os.path.basename(os.path.splitext(imageidbox.currentText())[0])
                     
                 )
             )            
+                 
+                    
                  
                  self.viewer.window.add_dock_widget(imageidbox, name="Image", area='left') 
                  self.viewer.window.add_dock_widget(eventidbox, name="Event", area='left')  
                  
                  
-                                     
-      
-                             
-                             
-          
-
-
-
                         
-class EventViewer(object):
-    
-    def __init__(self, viewer, image_toread, key_categories, imagename, savedir, canvas, ax, figure):
-        
-           
-           self.viewer = viewer
-           self.image_toread = image_toread
-           self.key_categories = key_categories
-           self.imagename = imagename
-           self.savedir = savedir
-           self.canvas = canvas
-           self.ax = ax
-           self.figure = figure
-           self.time = int(self.viewer.dims.point[0])
-           image = daskread(self.image_toread)
-           self.viewer.add_image(image, name= 'Image' + self.imagename )
-           
-           self.viewer.dims.events.current_step.connect(self.image_add)
-    def image_add(self, event):
-                            
-        print('in')
-        self.time = int(self.viewer.dims.point[0])
-        
-        print(self.time)
-        
-        for layer in list(self.viewer.layers):
-                                 if 'Image' in layer.name or layer.name in 'Image':
-                                            self.viewer.layers.remove(layer)
-                                            
-                                            
-        
-                                    
-        for (event_name,event_label) in self.key_categories.items():
-                        
-                        if event_label > 0 and self.event_name == event_name:
-        
-                             for layer in list(self.viewer.layers):
-                                  
-                                 if event_name in layer.name or layer.name in event_name or event_name + 'angle' in layer.name or layer.name in event_name + 'angle' :
-                                            self.viewer.layers.remove(layer)           
-                               
-                             
-                             csvname = self.savedir + "/" + event_name + "Location" + (os.path.splitext(os.path.basename(self.imagename))[0] + '.csv')    
-                             #event_locations, size_locations, timelist, eventlist = self.event_counter(csvname)
-                             
-                             #self.viewer.add_points(np.asarray(event_locations), size = size_locations ,name = event_name, face_color = [0]*4, edge_color = "red", edge_width = 1)
-                             
-                             
-                             
-                                      
-                             
-                             #self.viewer.theme = 'light'
-                             #self.ax.plot(timelist, eventlist, '-r')
-                             #self.ax.set_title(event_name + "Events")
-                             #self.ax.set_xlabel("Time")
-                             #self.ax.set_ylabel("Counts")
-                             #self.figure.canvas.draw()
-                             #self.figure.canvas.flush_events()
-                             #plt.savefig(self.savedir  + event_name   + '.png')
-
-                             
-    def event_counter(self, csv_file):
-     
-         data   = pd.read_csv(csv_file, delimiter = ',')
-         print(data(data["T"] == self.time))
-         time = data["T"]
-         y = data["Y"]
-         x = data["X"]
-         score = data["Score"]
-         size = data["Size"]
-         confidence = data["Confidence"] 
-         
-         
-         
-         
-         eventcounter = 0
-         eventlist = []
-         timelist = []   
-         listtime = time.tolist()
-         listy = y.tolist()
-         listx = x.tolist()
-         listsize = size.tolist()
-         listscore = score.tolist()
-         event_locations = []
-         size_locations = []
-         for i in (range(len(listtime))):
-             tcenter = int(listtime[i])
-             ycenter = listy[i]
-             xcenter = listx[i]
-             size = listsize[i]
-             print(tcenter)
-             eventcounter = listtime.count(tcenter)
-             timelist.append(tcenter)
-             eventlist.append(eventcounter)
-             event_locations.append([tcenter, ycenter, xcenter])   
-             size_locations.append(size)
-             
-             
+        def csv_add(self, imagename, csv_event_name ):
             
-         return event_locations, size_locations, timelist, eventlist                    
+            
+            self.event_name = csv_event_name
+            for (event_name,event_label) in self.key_categories.items():
+                                
+                                if event_label > 0 and csv_event_name == event_name:
+                                     
+                                     for layer in list(self.viewer.layers):
+                                          
+                                         if event_name in layer.name or layer.name in event_name or event_name + 'angle' in layer.name or layer.name in event_name + 'angle' :
+                                                    self.viewer.layers.remove(layer)           
+                                       
+                                     
+                                     self.csvname = self.savedir + "/" + event_name + "Location" + (os.path.splitext(os.path.basename(imagename))[0] + '.csv')
+                                     
+            self.viewer.dims.events.current_step.connect(self.update_slider)
+                                        
+                                     
+                                        
+            
+        def image_add(self, image_toread, imagename):
+                                    
+                for layer in list(self.viewer.layers):
+                                         if 'Image' in layer.name or layer.name in 'Image':
+                                                    self.viewer.layers.remove(layer)
+                image = daskread(image_toread)
+                if len(image.shape) > 3:
+                    image = image[0,:]
+                self.viewer.add_image(image, name= 'Image' + imagename )
+                
+                                                    
+                
+                                            
+        def update_slider(self, event):
+                  
+                        self.time = int(self.viewer.dims.point[0])        
+                        print('loop', self.time)           
+                        dataset   = pd.read_csv(self.csvname, delimiter = ',')
+                        #Data is written as T, Y, X, Score, Size, Confidence
+                        T = dataset[dataset.keys()[0]][1:].astype('int16')
+                        Y = dataset[dataset.keys()[1]][1:]
+                        X = dataset[dataset.keys()[2]][1:]
+                        Score = dataset[dataset.keys()[3]][1:]
+                        Size = dataset[dataset.keys()[4]][1:]
+                        Confidence = dataset[dataset.keys()[5]][1:]
+                        eventcounter = 0
+                        eventlist = []
+                        timelist = []   
+                        listtime = T.tolist()
+                        listy = Y.tolist()
+                        listx = X.tolist()
+                        listsize = Size.tolist()
+                        listscore = Score.tolist()
+                        event_locations = []
+                        size_locations = []
+                        for i in (range(len(listtime))):
+                             tcenter = int(listtime[i])
+                             ycenter = listy[i]
+                             xcenter = listx[i]
+                             size = listsize[i]
+                             eventcounter = listtime.count(tcenter)
+                             timelist.append(tcenter)
+                             eventlist.append(eventcounter)
+                             event_locations.append([tcenter, ycenter, xcenter])   
+                             size_locations.append(size)
+                                   
+                        self.ax.plot(timelist, eventlist, '-r')
+                        self.ax.set_title(self.event_name + "Events")
+                        self.ax.set_xlabel("Time")
+                        self.ax.set_ylabel("Counts")
+                        self.figure.canvas.draw()
+                        self.figure.canvas.flush_events()
+
+                        self.viewer.add_points(np.asarray(event_locations), size = size_locations ,name = self.event_name , face_color = [0]*4, edge_color = "red", edge_width = 1)
+                 
+                                    
